66517d75cdad9ebf1b6d8d3645a6e9e537b643e6
ChelovechekAD <yushkevich2004@mail.ru>
2021-03-19 00:01:35 +0300

    1

--- site/script/src.js
+++ site/script/src.js
@@ -1,30 +1,11 @@
-$(document).ready(function(){
-	$('.btn').click(function(event) {
-		$('.font_textarea').toggleClass('active')
-	});
-});
+
 $(document).ready(function(){
 	$('.textarea').click(function(event) {
-		$('.font_textarea').toggleClass('active')
-	});
-});
-$(document).ready(function(){
-	$('.btn0').click(function(event) {
-		$('.font_textarea').toggleClass('active')
-	});
-});
-$(document).ready(function(){
-	$('.btn').blur(function(event) {
-		$('.font_textarea').toggleClass('active')
+		$('.font_textarea').addClass('active')
 	});
 });
 $(document).ready(function(){
 	$('.textarea').blur(function(event) {
-		$('.font_textarea').toggleClass('active')
-	});
-});
-$(document).ready(function(){
-	$('.btn0').blur(function(event) {
-		$('.font_textarea').toggleClass('active')
+		$('.font_textarea').removeClass('active')
 	});
 });
