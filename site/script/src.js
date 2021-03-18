c806a21ee669009261891fdd06a9715d4c0639bd
ChelovechekAD <yushkevich2004@mail.ru>
2021-03-18 23:36:26 +0300

    1

---
+++ site/script/src.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.btn').click(function(event) {
+		$('.font_textarea').toggleClass('active')
+	});
+});
+$(document).ready(function(){
+	$('.textarea').click(function(event) {
+		$('.font_textarea').toggleClass('active')
+	});
+});
+$(document).ready(function(){
+	$('.btn0').click(function(event) {
+		$('.font_textarea').toggleClass('active')
+	});
+});
